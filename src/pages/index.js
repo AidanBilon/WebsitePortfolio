41c4214f9f1c7a14d66b839aec3c79287010aeda
AidanBilon <aidanbilon@gmail.com>
2023-08-07 15:22:16 -0400

    Google search file verification

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Aidan Bilon | Portfolio</title>
+        <meta name="google-site-verification" content="YOUR_VERIFICATION_CODE" />
         <meta name="description" content="Aidan Bilon's Website Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
