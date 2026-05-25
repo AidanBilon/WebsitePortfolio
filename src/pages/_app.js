dfc54afaa2c113d03accf7b9f1b6ea3191b0c08f
Aidan Bilon <aidanbilon@gmail.com>
2026-05-25 14:52:40 -0400

    Add Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import Navbar from '../../components/Navbar'
+import Script from 'next/script'
 
 function App({ Component, pageProps }) {
   return <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TTJH9QH72C" />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TTJH9QH72C');
+      `}
+    </Script>
     <Navbar />
     <Component {...pageProps} />
   </>
